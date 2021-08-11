717e0f9fa68e5eaf51934d66af09c1987802e9cf
NOGY <norven.norov@gmail.com>
2021-08-12 02:24:10 +0800

    :

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,6 @@ export default function Home() {
           This is a {variant} alert—check it out!
         </Alert>
       ))}
-
-      <Toast>
-        <Toast.Header>
-          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-          <strong className="me-auto">Bootstrap</strong>
-          <small>11 mins ago</small>
-        </Toast.Header>
-        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-      </Toast>
     </div>
   );
 }
