4fc3b1946772b1199ee4969c63cf13d5fe587747
NOGY <norven.norov@gmail.com>
2021-08-12 02:18:35 +0800

    react-bootstrap holboloo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Alert from "react-bootstrap/Alert";
+import Toast from "react-bootstrap/Toast";
 
 export default function Home() {
-  return <div className={styles.container}>My Blog</div>;
+  return (
+    <div className={styles.container}>
+      {[
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+      ].map((variant, idx) => (
+        <Alert key={idx} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+
+      <Toast>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+      </Toast>
+    </div>
+  );
 }
